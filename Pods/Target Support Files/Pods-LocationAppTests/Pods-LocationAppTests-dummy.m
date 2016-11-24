8f8a53b08d867bae3f94a946132752191cff9b86
Bruno Aybar <miguel_bruno@hotmail.com>
2016-11-24 21:35:08 +0000

    Setup pods

---
+++ Pods/Target Support Files/Pods-LocationAppTests/Pods-LocationAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LocationAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_LocationAppTests
+@end
---
+++ Pods/Target Support Files/Pods-LocationAppTests/Pods-LocationAppTests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_LocationAppTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LocationAppTestsVersionString[];
+
