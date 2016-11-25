9074edd4bb520e559263ba2af663be2cf68c2ae0
Bruno Aybar <miguel_bruno@hotmail.com>
2016-11-25 00:54:55 +0000

    Resize search text field

--- LocationApp/ViewController.swift
+++ LocationApp/ViewController.swift
@@ -11,21 +11,57 @@ import MapKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var bottomToolbar: UIToolbar!
+    @IBOutlet var searchTextField: UITextField!
+    @IBOutlet var searchButton: UIBarButtonItem!
     @IBOutlet var mapView: MKMapView!
     @IBOutlet var barToTableTopConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        actionTest(mapView)
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    @IBAction func actionTest(_ sender: Any) {
+    override func viewDidAppear(_ animated: Bool) {
+        setupSearchTextField()
+    }
+
+    // MARK: Search
+    
+    @IBAction func actionSearch(_ sender: Any) {
+        hideOrShowTableView()
+    }
+    
+    func setupSearchTextField(){
+        UIView.animate(withDuration: 0.5){
+            let toolbarWidth = self.bottomToolbar.frame.width
+            //Calculate search button size
+            var searchSize = 0
+            if let searchFrame = (self.searchButton.value(forKey: "view") as? UIView)?.frame{
+                searchSize = Int(toolbarWidth) - Int(searchFrame.origin.x)
+            }
+            //Set frame to fit available space on toolbar
+            var frame = self.searchTextField.frame
+            frame.size.width = toolbarWidth - CGFloat(searchSize + 16) - frame.origin.x
+            self.searchTextField.frame = frame
+        }
+    }
+    
+    func hideOrShowTableView() {
         barToTableTopConstraint.priority = barToTableTopConstraint.priority > 500 ? 250 : 900
         UIView.animate(withDuration: 0.5){
             self.view.layoutIfNeeded()
         }
-        
+    }
+    
+    // MARK: Location
+    
+    
+    @IBAction func actionLocation(_ sender: Any) {
+    }
+    
+    @IBAction func actionFilter(_ sender: Any) {
     }
     
     
